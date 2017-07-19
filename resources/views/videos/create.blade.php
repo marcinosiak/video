5b82ce1d98b48e6d2f107c78099b93df8bcaaa68
marcinosiak <om77@wp.pl>
2017-07-19 11:59:01 +0200

    Wydzielenie formularzy do osobnego pliku

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -6,56 +6,11 @@
             <div class="panel-body">
             <!-- Formularz -->
 
-              {{-- @include('videos.form_errors') --}}
-              @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                  <div>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                  </div>
-                </div>
-              @endif
-
+              @include('videos.form_errors')
 
             	{!! Form::open(['url' => 'videos', 'class' => 'form-horizontal']) !!}
 
-            	   {{-- @include('videos.form', ['buttonText' => 'Dodaj video']) --}}
-                 <div class="form-group">
-                     <div  class="col-md-4 control-label">
-                       {!! Form::label('title', 'Tytuł:') !!}
-                     </div>
-                     <div class="col-md-6">
-                         {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                     </div>
-                 </div>
-
-                 <div class="form-group">
-                     <div  class="col-md-4 control-label">
-                       {!! Form::label('description', 'Opis:') !!}
-                     </div>
-                     <div class="col-md-6">
-                         {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
-                     </div>
-                 </div>
-
-                 <div class="form-group">
-                     <div  class="col-md-4 control-label">
-                       {!! Form::label('url', 'Link do filmu:') !!}
-                     </div>
-                     <div class="col-md-6">
-                         {!! Form::text('url', null, ['class' => 'form-control']) !!}
-                     </div>
-                 </div>
-
-                 <div class="form-group">
-                     <div class="col-md-6 col-md-offset-4">
-                         {{-- {!! Form::submit($buttonText, ['class' => 'btn btn-primary']) !!} --}}
-                         {!! Form::submit("Dodaj artykuł", ['class' => 'btn btn-primary']) !!}
-                     </div>
-                 </div>
+            	   @include('videos.form', ['buttonText' => 'Dodaj video'])
 
               {!! Form::close() !!}
 
--- resources/views/videos/edit.blade.php
+++ resources/views/videos/edit.blade.php
@@ -6,56 +6,11 @@
             <div class="panel-body">
             <!-- Formularz -->
 
-              {{-- @include('videos.form_errors') --}}
-              @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                  <div>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                  </div>
-                </div>
-              @endif
-
+              @include('videos.form_errors')
 
             	{!! Form::model($video, ['method' => 'PATCH', 'class' => 'form-horizontal', 'action' => ['VideosController@update', $video->id]]) !!}
 
-            	   {{-- @include('videos.form', ['buttonText' => 'Dodaj video']) --}}
-                 <div class="form-group">
-                     <div  class="col-md-4 control-label">
-                       {!! Form::label('title', 'Tytuł:') !!}
-                     </div>
-                     <div class="col-md-6">
-                         {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                     </div>
-                 </div>
-
-                 <div class="form-group">
-                     <div  class="col-md-4 control-label">
-                       {!! Form::label('description', 'Opis:') !!}
-                     </div>
-                     <div class="col-md-6">
-                         {!! Form::textarea('description', null, ['class' => 'form-control']) !!}
-                     </div>
-                 </div>
-
-                 <div class="form-group">
-                     <div  class="col-md-4 control-label">
-                       {!! Form::label('url', 'Link do filmu:') !!}
-                     </div>
-                     <div class="col-md-6">
-                         {!! Form::text('url', null, ['class' => 'form-control']) !!}
-                     </div>
-                 </div>
-
-                 <div class="form-group">
-                     <div class="col-md-6 col-md-offset-4">
-                         {{-- {!! Form::submit($buttonText, ['class' => 'btn btn-primary']) !!} --}}
-                         {!! Form::submit("Dodaj artykuł", ['class' => 'btn btn-primary']) !!}
-                     </div>
-                 </div>
+            	   @include('videos.form', ['buttonText' => 'Aktualizuj video'])
 
               {!! Form::close() !!}
 
